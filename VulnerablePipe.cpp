d698ce84be2ec5565641fdaf987b2048f581e6b3
Danisy Eisyraf <64330566+DanisyEisyraf@users.noreply.github.com>
2024-04-16 18:28:09 +0800

    Update VulnerablePipe.cpp

--- VulnerablePipe.cpp
+++ VulnerablePipe.cpp
@@ -49,8 +49,6 @@ int main() {
     std::cout << "   \\_/  \\__,_|_|_| |_|\\___|_|  \\__,_|_.__/|_|\\___|   \\____/\\___/ \\__, |_|_| |_|  \\/    \\__,_|\\__, |\\___| " << std::endl;
     std::cout << "                                                                 |___/                       |___/      " << std::endl;
     std::cout << "\n\n\n\n\n";
-
-    // Reset color to bright white
     SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 
     std::cout << "Enter your TP Number: ";
@@ -58,8 +56,10 @@ int main() {
     std::cout << "Enter your password: ";
     std::cin >> password;
 
+    //SQL Injection command: admin' OR '1'='1'--
     std::string dataToSend = TPNo + "|" + password;
     std::string deviceInfo = getDeviceInfo(TPNo, password);
+
 
 
 
@@ -95,9 +95,43 @@ int main() {
         std::cout << "Pipe creation successful." << std::endl;
     }
 
-    // Connect to pipe
+//////////////////////////////Calling the Client_Handler.py////////////////////////
+    // Path to the Python interpreter
+    std::wstring pythonInterpreter = L"python";
+    std::wstring pythonScript = L"Insert Directory Here";
+    std::wstring commandLineArgs = L"";
+    // Create process parameters
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+    // Initialize structures
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Concatenate command line
+    std::wstring command = pythonInterpreter + L" " + pythonScript + L" " + commandLineArgs;
+
+    // Create process
+    if (!CreateProcessW(NULL,   // No module name (use command line)
+        const_cast<LPWSTR>(command.c_str()), // Command line
+        NULL,       // Process handle not inheritable
+        NULL,       // Thread handle not inheritable
+        FALSE,      // Set handle inheritance to FALSE
+        0,          // No creation flags
+        NULL,       // Use parent's environment block
+        NULL,       // Use parent's starting directory
+        &si,        // Pointer to STARTUPINFO structure
+        &pi)        // Pointer to PROCESS_INFORMATION structure
+        ) {
+        std::cerr << "CreateProcess failed: " << GetLastError() << std::endl;
+        return 1;
+    }
+/////////////////////////End of Calling/////////////////////////////
+
+
     std::cout << "Connecting to pipe..." << std::endl;
     bConnectNamedPipe = ConnectNamedPipe(hCreateNamedPipe, NULL);
+
     if (bConnectNamedPipe == FALSE) {
         SetConsoleColor(FOREGROUND_RED);
         std::cout << "Failed to connect to pipe. Error no: " << GetLastError() << std::endl;
@@ -107,6 +141,8 @@ int main() {
         std::cout << "Successfully connected to Pipe" << std::endl;
     }
 
+
+    ////////////////////////////////////////////PIPE DATA SENDING//////////////////////
     // Write to Pipe
     DWORD dwNoBytesWrite;
     BOOL bWriteFile = WriteFile(
@@ -127,46 +163,51 @@ int main() {
         std::cout << "Credentials sent to pipe" << std::endl;
     }
 
-
-    // Write device information to Pipe
-    BOOL cWriteFile = WriteFile(
-        hCreateNamedPipe,
-        deviceInfo.c_str(),
-        deviceInfo.length(),
-        &dwNoBytesWrite,
-        NULL
-    );
-
-    if (cWriteFile == FALSE) {
+    DWORD dwBytesRead;
+    if (ReadFile(hCreateNamedPipe, szInputBuffer, sizeof(szInputBuffer), &dwBytesRead, NULL)) {
+        // Print the received message
+        std::string message(szInputBuffer);
+        if (message.find("User does not exist!") != std::string::npos) {
+            SetConsoleColor(FOREGROUND_RED);
+            std::cout << "User Does Not Exist! Aborting..." << std::endl;
+            return 1;
+        }
+    }
+    else {
         SetConsoleColor(FOREGROUND_RED);
-        std::cout << "Failed to update status to pipe" << std::endl;
+        std::cout << "Failed to read from the named pipe. Error code: " << GetLastError() << std::endl;
     }
 
-    SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-    std::cout << "\nStatus Updated to pipe" << std::endl;
+
+    Sleep(500);
+   /////End of Credentials
 
 
+
+    //Start of LOGS
+    // Write device information to Pipe
     while (true)
     {
-        std::string deviceInfo = getDeviceInfo(TPNo, password);
-        Sleep(4000);
-
-        BOOL statusWriteFile = WriteFile(
-
+        ConnectNamedPipe(hCreateNamedPipe, NULL);
+        deviceInfo = getDeviceInfo(TPNo, password);
+        DWORD dwLogs;
+        BOOL cWriteFile = WriteFile(
             hCreateNamedPipe,
             deviceInfo.c_str(),
             deviceInfo.length(),
-            &dwNoBytesWrite,
+            &dwLogs,
             NULL
         );
 
-        FlushFileBuffers(hCreateNamedPipe);
-        std::cout << "\n" << deviceInfo << std::endl;
-
-
+        if (cWriteFile == FALSE) {
+            SetConsoleColor(FOREGROUND_RED);
+            std::cout << "Server: Failed to update status to pipe" << std::endl;
+        }
+        SetConsoleColor(FOREGROUND_BLUE);
+        std::cout << "\nServer: Status sent to pipe" << std::endl;
+        SetConsoleColor(FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        DisconnectNamedPipe(hCreateNamedPipe);
+        Sleep(2000);
     }
-
-
-
     return 0;
 }
